ca015d6d7d48d77a12a0a29c8bc3c59b52fae0a6
Peter <peteremil791@gmail.com>
2026-07-20 22:05:27 +0300

    added calculator to the application with 2 operators

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [result, setResult] = useState(0);
+  const [operator, setOperator] = useState("+");
+  const HandleEnter = () => {
+    // console.log(first, second); debugging
+    const no1 = parseInt(first);
+    const no2 = parseInt(second);
+    setResult(operator === "+" ? no1 + no2 : no1 - no2);
+  };
+  useEffect(() => {
+    const handleKeyUp = (e) => {
+      if (e.key === "Enter") {
+        HandleEnter();
+      }
+    };
+
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  });
+
+  return (
+    <>
+      <div>
+        <h1>Calculator</h1>
+        <div>
+          <input
+            type="number"
+            value={first}
+            onChange={(e) => setFirst(e.target.value)}
+          />
+          <input
+            type="number"
+            value={second}
+            onChange={(e) => setSecond(e.target.value)}
+          />
+          <input
+            type="number"
+            value={result}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                // console.log(first, second); debugging
+                const no1 = parseInt(first);
+                const no2 = parseInt(second);
+                setResult(operator === "+" ? no1 + no2 : no1 - no2);
+              }
+            }}
+          />
+          <button onClick={() => setOperator("+")}>+</button>
+          <button onClick={() => setOperator("-")}>-</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
